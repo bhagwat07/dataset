23ad51c3ac991341dd59fff56b5d20fa0379f086
Bhagwat <bhagwatkakde99@gmail.com>
2023-02-06 08:27:33 +0530

    adding a test2.py file

---
+++ test2.py
@@ -0,0 +1,14 @@
+
+
+from pyspark.sql import *
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.master["local[*]"].appName("git").getOrCreate()
+
+
+
+data = "C:\\Bigdata\\driver\\emp.csv"
+
+df = spark.read.format('csv').option("header","true").load(data)
+
+df.show()
